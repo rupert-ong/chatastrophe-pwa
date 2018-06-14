a3eeaa22999eed372f5e411bea344b75ecdb2234
rupert-ong <rupert.ong@gmail.com>
2018-06-14 14:40:50 -0400

    Add Webpack config files

---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+const webpack = require('webpack');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: [
+    'react-hot-loader/patch',
+    './src/index.js'
+  ],
+
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
+  },
+
+  resolve: {
+    extensions: ['*', '.js', '.jsx']
+  },
+
+  output: {
+    path: __dirname + '/public',
+    filename: 'bundle.js',
+    publicPath: '/'
+  },
+
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new HTMLWebpackPlugin({
+      inject: true,
+      template: __dirname + '/public/index.html'
+    })
+  ],
+
+  devServer: {
+    contentBase: "./public",
+    historyApiFallback: true,
+    hot: true
+  }
+};
---
+++ webpack.config.prod.js
@@ -0,0 +1,72 @@
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const ManifestWebpackPlugin = require('webpack-manifest-plugin');
+
+module.exports = {
+  entry: [
+    './src/index.js'
+  ],
+
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.(ico|png|svg|jpg|gif)$/,
+        use: [{
+          loader: 'file-loader',
+          options: {
+            name: 'static/media/[name].[ext]'
+          }
+        }],
+      }
+    ]
+  },
+
+  resolve: {
+    extensions: ['*', '.js', '.jsx']
+  },
+
+  output: {
+    path: __dirname + '/build',
+    filename: 'bundle.js',
+    publicPath: './'
+  },
+
+  plugins: [
+    new HTMLWebpackPlugin({
+      inject: true,
+      template: __dirname + '/public/index.html',
+      minify: {
+        removeComments: true,
+        collapseWhitespace: true,
+        removeRedundantAttributes: true,
+        useShortDoctype: true,
+        removeEmptyAttributes: true,
+        removeStyleLinkTypeAttributes: true,
+        keepClosingSlash: true,
+        minifyJS: true,
+        minifyCSS: true,
+        minifyURLs: true
+      }
+    }),
+    new CopyWebpackPlugin([
+      {
+        from: __dirname + '/public/',
+        to: '',
+        ignore: ['*.html', 'secrets-template.js'],
+        toType: 'dir'
+      }
+    ]),
+    new ManifestWebpackPlugin({
+      fileName: 'asset-manifest.json'
+    })
+  ]
+};
