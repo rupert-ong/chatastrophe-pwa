cd6f8dd695e444b069bfc87ba60fb39367281b2f
rupert-ong <rupert.ong@gmail.com>
2018-06-15 15:34:45 -0400

    Fix asset link in Header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Header = () => {
   return (
     <div id="Header">
-      <img src="/assets/icon.png" alt="logo" />
+      <img src="assets/icon.png" alt="logo" />
       <h1>Chatastrophe</h1>
     </div>
   );
