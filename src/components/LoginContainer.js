517dbaaecb0e56e49bf9c0c772b3d1e49695dcd5
rupert-ong <rupert.ong@gmail.com>
2018-06-15 21:02:17 -0400

    Add Firebase login and signup methods

--- src/components/LoginContainer.js
+++ src/components/LoginContainer.js
@@ -6,6 +6,27 @@ class LoginContainer extends Component {
 
   state = { email: '', password: '', error: '' };
 
+  login = (email, password) => {
+    firebase.auth().signInWithEmailAndPassword(email, password)
+      .then(res => console.log(res))
+      .catch(err => {
+        if (err.code === 'auth/user-not-found') {
+          this.signUp(email, password);
+        } else {
+          this.setState({ error: err.message });
+        }
+      });
+  }
+
+  signUp = (email, password) => {
+    firebase.auth().createUserWithEmailAndPassword(email, password)
+      .then(res => console.log(res))
+      .catch(err => {
+        console.log(err);
+        this.setState({ error: err.message });
+      });
+  }
+
   handleEmailChange = e => {
     this.setState({
       email: e.target.value
@@ -18,11 +39,13 @@ class LoginContainer extends Component {
     })
   }
 
-  handleSubmit =  e => {
+  handleSubmit = e => {
     const { email, password } = this.state;
     e.preventDefault();
-    if(email && password) {
-      // try login or signup
+
+    if (email && password) {
+      this.setState({ error: '' });
+      this.login(email, password);
     } else {
       this.setState({ error: 'Please fill in both fields' });
     }
@@ -37,17 +60,17 @@ class LoginContainer extends Component {
           <p>Sign in or sign up by entering your email and password.</p>
           <input
             type="text"
-            onChange={this.handleEmailChange} 
-            value={email} 
+            onChange={this.handleEmailChange}
+            value={email}
             placeholder="Your email" />
           <input
             type="password"
-            onChange={this.handlePasswordChange} 
-            value={password} 
+            onChange={this.handlePasswordChange}
+            value={password}
             placeholder="Your password" />
-          { error && (
-           <p className="error">{error}</p> 
-          ) }
+          {error && (
+            <p className="error">{error}</p>
+          )}
           <button className="red light" type="submit">Login</button>
         </form>
       </div>
