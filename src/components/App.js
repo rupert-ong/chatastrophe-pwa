05df46bc11740e209236d4ebaddac27941016932
rupert-ong <rupert.ong@gmail.com>
2018-06-15 21:07:12 -0400

    Save Firebase user object on and after successful login in App component

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,16 @@ import './App.css';
 import LoginContainer from './LoginContainer'
 
 export default class App extends Component {
+  state = { user: null }
+
+  componentDidMount() {
+    firebase.auth().onAuthStateChanged(user => {
+      if (user) {
+        this.setState({ user });
+      }
+    })
+  }
+
   render() {
     return (
       <div id="container" className="inner-container">
