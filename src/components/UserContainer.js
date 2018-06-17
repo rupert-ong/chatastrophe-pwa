af1722b6d43e8f89f018aec57eb8c4efd437de51
rupert-ong <rupert.ong@gmail.com>
2018-06-16 22:32:32 -0400

    Add back to chat link to User component

--- src/components/UserContainer.js
+++ src/components/UserContainer.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
 import Header from './Header';
 
 export default class UserContainer extends Component {
   render() {
     return (
       <div id="UserContainer">
-        <Header />
+        <Header>
+          <Link to="/">
+            <button className="red">Back to Chat</button>
+          </Link>
+        </Header>
         <h1>Hello from User Container {this.props.match.params.id}</h1>
       </div>
     );
