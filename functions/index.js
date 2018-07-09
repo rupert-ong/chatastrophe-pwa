cd6651d14d4e936ec577b5bff306192191c26f16
rupert-ong <rupert.ong@gmail.com>
2018-07-08 23:10:41 -0400

    Add cloud function listener for onWrite events on message DB.

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,26 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+admin.initializeApp(functions.config().firebase);
+
+// Listen for onWrite (new message) events of messages store
+exports.sendNotifications = functions.database
+  .ref('/messages/{messageId}')
+  .onWrite(event => {
+    const snapshot = event.data;
+
+    // No support for updated/edited messages
+    if (snapshot.previous.val()) {
+      return;
+    }
+
+    // Notification object
+    const payload = {
+      notification: {
+        title: `${snapshot.val().author}`,
+        body: `${snapshot.val().msg}`,
+        icon: 'assets/icon.png',
+        click_action: `https://${functions.config().firebase.authDomain}`
+      }
+    }
+  });
